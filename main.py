080ad8ece56a51005eff743a1d449016c8bfe4b5
Sai Venkat <saivenkatyalamanchi@gmail.com>
2026-06-21 18:06:48 +0530

    structured output

--- main.py
+++ main.py
@@ -4,8 +4,19 @@ from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_ollama import ChatOllama
 from tavily import TavilyClient
+from typing import List
+from pydantic import BaseModel, Field
 
 load_dotenv()
+
+class Source(BaseModel):
+    """Schema for a source used by the agent."""
+    url : str = Field(description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """Schema for a response from the agent."""
+    answer : str = Field(description="The answer from the agent to the query")
+    sources : List[Source] = Field(default_factory=list, description="The sources used by the agent to arrive at the answer")
 
 tavily_client = TavilyClient()
 
@@ -23,7 +34,7 @@ def search(query : str) -> str:
 
 llm = ChatOllama(model="qwen3:8b", temperature=0)
 tools = [search]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 def main():
     print("Hello from langchain-course!")
